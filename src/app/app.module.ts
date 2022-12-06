4134f7d3c5cbd051a50281cc9e75a528d5ceed9e
ashwiii <achuzbro@gmail.com>
2022-12-06 14:41:08 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiteLayoutModule } from './site-layout/site-layout.module';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AfterloginComponent } from './afterlogin/afterlogin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    AfterloginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SiteLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
