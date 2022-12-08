219870e46cfe01a0b00d3cb2b0101c08ef39d7bd
ashwiii <achuzbro@gmail.com>
2022-12-08 11:36:53 +0530

    14

--- src/app/select-category/select-category.component.ts
+++ src/app/select-category/select-category.component.ts
@@ -12,4 +12,20 @@ export class SelectCategoryComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  countDownDate= new Date("december 30,2022 15:37:27").getTime();
+demo:any
+  x = setInterval(()=>{
+    var now = new Date().getTime();
+    var distance = this.countDownDate - now
+    var days = Math.floor(distance/(1000*60*60*24));
+    var hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+    var minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+    var seconds= Math.floor((distance % (1000*60)) / 1000);
+    this.demo = days + "d "+hours + "h " + minutes + "m "+seconds + "s";
+    if(distance<0){
+      clearInterval(this.x)
+      this.demo="Expired"
+    }
+  })
+
 }
--- src/app/terms/terms.component.ts
+++ src/app/terms/terms.component.ts
@@ -12,4 +12,7 @@ export class TermsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  
+
 }
